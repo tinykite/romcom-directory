8a77274b59d46ffe1e38dae4437b923797113965
tinykite <dakota@tinykitelab.com>
2021-05-26 17:19:10 -0500

    Add header and sidebar components

--- src/components/UserDirectory.js
+++ src/components/UserDirectory.js
@@ -12,22 +12,26 @@ const sendUserRequest = async () => {
 };
 
 const formatUserData = ({ data }) => {
-  return data.map((user) => {
+  return data.map((user, i) => {
     const { name, phone, email, address } = user;
+    const title = i < 5 ? "Magazine Editor" : "Architect";
+    const department = i > 5 ? "Design" : "Art";
     return {
       name,
       phone,
       email,
       location: address.city,
-      title: "Assistant",
-      department: "Creative",
+      title,
+      department,
     };
   });
 };
 
-const DirectoryContainer = styled.div`
-  margin: 0 auto;
-`;
+const sortUserData = ({ data }) => {
+  return data;
+};
+
+const DirectoryContainer = styled.div``;
 
 const CardContainer = styled.article`
   background: #f8f8f8;
@@ -38,6 +42,17 @@ const CardContainer = styled.article`
   & + & {
     margin-top: 1rem;
   }
+`;
+
+const DirectorySortHeading = styled.h3`
+  margin-top: 0.5rem;
+`;
+
+const DirectorySortList = styled.ul``;
+
+const DirectorySortListItem = styled.li`
+  cursor: pointer;
+  padding: 0.25rem;
 `;
 
 const Card = ({ user }) => {
@@ -54,8 +69,61 @@ const Card = ({ user }) => {
   );
 };
 
+const getCategoryHeadings = (users) => {
+  let allLocations = [];
+  let allDepartments = [];
+  let allTitles = [];
+
+  // Create lists for every location, department, and title
+  users.forEach((user) => {
+    allLocations.push(user.location);
+    allDepartments.push(user.department);
+    allTitles.push(user.title);
+  });
+
+  // Remove duplicates using a set
+  const uniqueLocations = new Set(allLocations);
+  const uniqueDepartments = new Set(allDepartments);
+  const uniqueTitles = new Set(allTitles);
+
+  // Return each list as a regular old array
+  return [
+    {
+      heading: "Department",
+      data: [...uniqueDepartments],
+    },
+    {
+      heading: "Title",
+      data: [...uniqueTitles],
+    },
+    {
+      heading: "Location",
+      data: [...uniqueLocations],
+    },
+  ];
+};
+
+const CategoryList = ({ category }) => {
+  const { heading, data } = category;
+  return (
+    <>
+      <DirectorySortHeading>{heading}</DirectorySortHeading>
+      <DirectorySortList>
+        {data.map((item) => (
+          <DirectorySortListItem>{item}</DirectorySortListItem>
+        ))}
+      </DirectorySortList>
+    </>
+  );
+};
+
 const UserDirectory = () => {
   const [userDirectory, setUserDirectory] = useState([]);
+  const [sortPreference, setSortPreference] = useState(null);
+  const sortedData = sortUserData(userDirectory, sortPreference);
+
+  const categoryHeadings =
+    userDirectory.length && getCategoryHeadings(userDirectory);
 
   useEffect(() => {
     sendUserRequest()
@@ -64,11 +132,34 @@ const UserDirectory = () => {
   }, []);
 
   return userDirectory.length ? (
-    <DirectoryContainer>
-      {userDirectory.map((user, i) => (
-        <Card user={user} key={`user_${i}`} />
-      ))}
-    </DirectoryContainer>
+    <>
+      <header style={{ margin: "2rem auto", textAlign: "center" }}>
+        <h1>User Directory</h1>
+        <em>
+          Just magazine editors and architects for now. Basically the cast of
+          every romantic comedy ever.
+        </em>
+      </header>
+      <main
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "2rem",
+        }}
+      >
+        <div>
+          <h2>Sort By</h2>
+          {categoryHeadings.map((category, i) => (
+            <CategoryList category={category} />
+          ))}
+        </div>
+        <DirectoryContainer>
+          {userDirectory.map((user, i) => (
+            <Card user={user} key={`user_${i}`} />
+          ))}
+        </DirectoryContainer>
+      </main>
+    </>
   ) : (
     "Loading"
   );
